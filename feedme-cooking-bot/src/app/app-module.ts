678d5cbe455bd8646243729215f304b5fba13718
VictorDoom022 <victordoom022@gmail.com>
2025-08-20 00:26:00 +0800

    feat: install BootstrapCSS

--- feedme-cooking-bot/src/app/app-module.ts
+++ feedme-cooking-bot/src/app/app-module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './pages/home/home';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { Home } from './pages/home/home';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
